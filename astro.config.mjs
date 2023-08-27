9ab843ce95cf0f27dac0e2577882144cfcc0a60c
DeathsValentine <39684769+DeathsValentine@users.noreply.github.com>
2023-08-27 13:42:43 -0700

    logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: netlify(),
 });
